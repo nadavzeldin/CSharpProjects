bea4da1bf6e4287da605b2653fd8704244017bc0
nadav <nadavzeldin@gmail.com>
2020-08-15 16:14:54 +0300

    Create Solution and project in correct formate

---
+++ C20 Ex01 Shir 307944710 Nadav 311550230/C20_EX01_01/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C20_EX01_01
+{
+    class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
